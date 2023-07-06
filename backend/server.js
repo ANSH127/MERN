41c08cce0931684ddd3a2f075e04a1ec93def5ee
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-06 22:19:31 +0530

    connecting to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,36 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutsRoutes = require('./routes/workouts');
 // express app 
 const app = express();
 
 // middleware
 app.use((req, res, next) => {
-    console.log(req.path,req.method);
+    console.log(req.path, req.method);
     next();
 });
 
 // parse json data
 app.use(express.json());
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`)
-});
 
 // routes
 
 app.use('/api/workouts', workoutsRoutes);
+
+
+// connect to mongodb 
+const dbURI = process.env.MONGO_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on port ${process.env.PORT}`)
+        });
+
+    })
+    .catch((err) => console.log(err));
 
 
