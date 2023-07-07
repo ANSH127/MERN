1fb43a4acbeb5980d1dfc108225c45ab137d8f30
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-07 14:02:36 +0530

    deleting data

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,25 @@
 import React from 'react'
-
+import useWorkoutsContext from '../hooks/useWorkoutsContext'
 export default function WorkoutDetails({ workout}) {
+    const {dispatch} = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const res = await fetch(`/api/workouts/${workout._id}`, {
+            method: 'DELETE'
+        })
+        const data = await res.json()
+        if(res.ok) {
+            console.log(data)
+            dispatch({type: 'DELETE_WORKOUT', payload: data.workout})
+        }
+    }
   return (
     <div className="workout-details">
         <h4>{workout.title}</h4>
         <p><strong>Load (Kg):</strong>{workout.load}</p>
         <p><strong>Reps :</strong>{workout.reps}</p>
         <p>{workout.createdAt}</p>
+        <span onClick={handleClick} >delete</span>
 
     </div>
   )
--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -9,6 +9,9 @@ const workoutsReducer = (state, action) => {
         
         case 'CREATE_WORKOUT':
             return {workouts: [...state.workouts, action.payload]}
+        
+        case 'DELETE_WORKOUT':
+            return {workouts: state.workouts.filter((workout) => workout._id !== action.payload._id)}
         default:
             return state
     }
