5e6998cc7b8fc8cfd51fe5850abc4ecf703f07d8
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-07 11:15:25 +0530

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  // Link
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+
+
+    </div>
+  );
+}
+
+export default App;
