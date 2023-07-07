46494bcdcbe542fa54e36653145971b895fce7e2
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-07 15:01:05 +0530

    user routes,controller,model setup

---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const UserControllers = require('../controllers/userController');
+// login route
+router.post('/login', UserControllers.loginUser);
+
+// signup route
+router.post('/signup', UserControllers.signupUser);
+
+
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutsRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 // express app 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 // routes
 
 app.use('/api/workouts', workoutsRoutes);
+app.use('/api/user', userRoutes);
 
 
 // connect to mongodb 
