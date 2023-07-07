ad1faf5c84d745fc34eecc280c72f326acff8209
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-07 14:26:31 +0530

    updating files

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import useWorkoutsContext from '../hooks/useWorkoutsContext'
-export default function WorkoutDetails({ workout}) {
-    const {dispatch} = useWorkoutsContext()
+
+// date-fns
+import { formatDistanceToNow } from 'date-fns'
+export default function WorkoutDetails({ workout }) {
+    const { dispatch } = useWorkoutsContext()
 
     const handleClick = async () => {
         const res = await fetch(`/api/workouts/${workout._id}`, {
             method: 'DELETE'
         })
         const data = await res.json()
-        if(res.ok) {
+        if (res.ok) {
             console.log(data)
-            dispatch({type: 'DELETE_WORKOUT', payload: data.workout})
+            dispatch({ type: 'DELETE_WORKOUT', payload: data.workout })
         }
     }
-  return (
-    <div className="workout-details">
-        <h4>{workout.title}</h4>
-        <p><strong>Load (Kg):</strong>{workout.load}</p>
-        <p><strong>Reps :</strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-        <span onClick={handleClick} >delete</span>
+    return (
+        <div className="workout-details">
+            <h4>{workout.title}</h4>
+            <p><strong>Load (Kg):</strong>{workout.load}</p>
+            <p><strong>Reps :</strong>{workout.reps}</p>
+            <p>
+                {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+            </p>
+            <span className='material-symbols-outlined' onClick={handleClick} >
+                delete
+            </span>
 
-    </div>
-  )
+        </div>
+    )
 }
