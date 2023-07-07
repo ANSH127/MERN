cbd35aaba118f615fc445a1fc7dff567dd1096f8
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-07 15:30:08 +0530

    email & pass validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 
 const userSchema = new mongoose.Schema({
@@ -7,6 +8,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+
     },
     password: {
         type: String,
@@ -17,6 +19,18 @@ const userSchema = new mongoose.Schema({
 
 // static signup method
 userSchema.statics.signup = async function(email, password) {
+
+    // validation
+    if(!email || !password){
+        throw Error("Email and password are required")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Email is invalid")
+    }
+    if(!validator.isstrongPassword(password)){
+        throw Error("Password is not strong enough")
+    }
+
     const exists=await this.findOne({email})
     if(exists){
         throw Error("Email already exists")
