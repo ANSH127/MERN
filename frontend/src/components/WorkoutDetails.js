8e39135c27310ce97a97db0b76995c320b1d6151
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-07-07 11:39:14 +0530

    fetching data

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function WorkoutDetails({ workout}) {
+  return (
+    <div className="workout-details">
+        <h4>{workout.title}</h4>
+        <p><strong>Load (Kg):</strong>{workout.load}</p>
+        <p><strong>Reps :</strong>{workout.reps}</p>
+        <p>{workout.createdAt}</p>
+
+    </div>
+  )
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { useEffect } from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
 
 export default function Home() {
+  const [workouts, setWorkouts] = React.useState([])
+
+  const fetchWorkouts = async () => {
+    const res = await fetch('/api/workouts/')
+    const data = await res.json()
+    if(res.ok) {
+      console.log(data)
+      setWorkouts(data.workouts)
+    }
+
+  }
+
+  useEffect(() => {
+    fetchWorkouts()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="workouts">
+        {workouts?.map((workout)=>{
+          return (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          )
+        })}
+      </div>
+    </div>
   )
 }
